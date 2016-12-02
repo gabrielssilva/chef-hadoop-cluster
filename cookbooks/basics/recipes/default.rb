ca93ff21b3f1b8d1313354b22ebd9bf09a691b6b
Gabriel Silva <gabriel93.silva@gmail.com>
2016-12-01 23:50:58 -0200

    Setups a basic hadoop installation on every node
    Signed-off-by: Gabriel Silva <gabriel93.silva@gmail.com>

--- cookbooks/basics/recipes/default.rb
+++ cookbooks/basics/recipes/default.rb
@@ -1,1 +1,12 @@
 package 'vim'
+package 'links'
+package 'default-jdk'
+
+tar_extract 'http://ftp.unicamp.br/pub/apache/hadoop/common/hadoop-2.7.3/hadoop-2.7.3.tar.gz' do
+  target_dir '/usr/share/'
+end
+
+cookbook_file '/usr/share/hadoop-2.7.3/etc/hadoop/hadoop-env.sh' do
+  owner 'vagrant'
+  mode '0755'
+end
